ed742f8c681dd09d32b40f1ecb04c14c8d7f009f
Shuja Ali <37407188+shujaalik@users.noreply.github.com>
2025-05-28 08:24:42 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/custom/header'
 
 function App() {
   return (
-    <div className="flex justify-center items-start flex-col px-8 gap-5">
+    <div className="flex justify-center items-start flex-col px-10 py-4 gap-5">
       <Header />
     </div>
   )
